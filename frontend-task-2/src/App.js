6b1a57443f13c21ed847915cc2a3fe146d555597
Rishu Kumar Nayak <57229844+rishunayak@users.noreply.github.com>
2023-07-19 15:14:44 +0530

    Updated Search functionality

--- frontend-task-2/src/App.js
+++ frontend-task-2/src/App.js
@@ -1,27 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios"
-import { Box, Button, Center, Heading, Image, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Heading, Image, Input, Text } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [qury,setQury]=useState("")
   const [data,setData]=useState({})
   const [page,setPage]=useState(1)
-  const {videoData,currentPage,totalPage}=data
+  const {currentPage,totalPage}=data
 
   useEffect(()=>
   {
-    axios.get(`http://localhost:3001/getVideo?page=${page}`).then(r=>setData(r.data))
+    axios.get(`https://hack2skillbackend2.onrender.com/getVideo?page=${page}`).then(r=>setData(r.data))
 
   },[page])
-  console.log(videoData)
+
+  const handleSearch=()=>
+  {
+     axios.get(`https://hack2skillbackend2.onrender.com/searchVideo?query=${qury}`).then(r=>setData(r.data))
+  }
+ console.log(data)
   return (
    <>
+   <Box width={"50%"}  display={"flex"} m={"auto"} mt="20px"  >
+    <Input placeholder='Search' type='text' value={qury} onChange={(e)=>setQury(e.target.value)}/><Button onClick={handleSearch}>Search</Button>
+   </Box>
 
      <Box display={"grid"} gridTemplateColumns={["repeat(1,1fr)","repeat(2,1fr)","repeat(3,1fr)","repeat(3,1fr)"]} gap={"10px"} padding={"10px"}>
        {
-       videoData?.map((ele)=>
+       data.videoData?.map((ele)=>
        <Box> 
          <Image src={ele.thumbnails.high.url} width={"100%"}/>
          <Text fontSize="12px">Publised On :- {ele.publishedDate}</Text>
